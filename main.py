fe4a14e28f2837da3922825c759ea729784dc3be
MatthewCompSci <costam@wellingtoncollege.org.uk>
2024-10-02 11:51:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-#hello
-print()
+import time
+
+class Tamagotchi:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.happiness = 100
+        self.hygiene = 100
+        self.age = 0
+        self.weight = 5
+        self.intelligence = 50
+        self.last_interaction_time = time.time()
