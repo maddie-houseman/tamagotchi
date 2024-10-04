fb1003f815f1b67c2f0ec27e721905b20286f6eb
maddie-houseman <HOUSEMM@wellingtoncollege.org.uk>
2024-10-04 10:54:27 +0100

    hi

--- First.py
+++ First.py
@@ -3,17 +3,17 @@ import time
 class Tamagotchi:
     def __init__(self, name):
         self.name = name
-        self.health = 100 # 100 (high) - 0 (low)
-        self.happiness = 100 # 100 (high) - 0 (low)
-        self.hygiene = 100 # 100 (high) - 0 (low)
-        self.age = 0 # 0 (low) - 75 (high)
-        self.weight = 5 # 
-        self.intelligence = 50 # 50 (base) - 150 (highest)
-        self.hunger = 0 # 0 (low) - 10 (starving)
-        self.strength = 0 # 0 (lowest)
-        self.last_interaction_time = time.time()
+        self.health = 100
+        self.happiness = 100
+        self.hygiene = 100
+        self.age = 0
+        self.weight = 5
+        self.intelligence = 50
+        self.hunger = 0
+        self.strength = 10
+        self.last_interaction_time = time.time() 
 
-def update(self):
+    def update(self):
         current_time = time.time()
         time_elapsed = current_time - self.last_interaction_time
 
@@ -30,7 +30,7 @@ def update(self):
         
         return self.happiness
 
-def feed(self, food):
+    def feed(self, food):
         self.update()
         if food == 'healthy':
             self.health += 5
@@ -41,33 +41,28 @@ def feed(self, food):
             self.health -= 5
         self.check_limits()
 
-def wash(self):
+    def wash(self):
         self.update()
         self.hygiene = 100
         self.check_limits()
 
-def play(self):
+    def play(self):
         self.update()
         self.happiness += 10
         self.hygiene -= 10
         self.check_limits()
 
-def sleep(self):
+    def sleep(self):
         self.update()
         self.health += 10  # Sleeping improves health
         self.check_limits()
 
-def doctor(self):
+    def doctor(self):
      self.update()
      self.health = 100
      self.check_limits()
 
-def workout(self):
-     self.update()
-     self.strength += 1
-     self.check_limits()
-
-def check_limits(self):
+    def check_limits(self):
         if self.health > 100:
             self.health = 100
         elif self.health < 0:
@@ -83,19 +78,107 @@ def check_limits(self):
         elif self.hygiene < 0:
             self.hygiene = 0
 
-import json
+    def name_getter(self):
+        return self.name
+    
+    def health_getter(self):
+        return self.health
+    
+    def happiness_getter(self):
+        return self.happiness
+    
+    def hygiene_getter(self):
+        return self.hygiene
+    
+    def age_getter(self):
+        return self.age
+    
+    def weight_getter(self):
+        return self.weight
+    
+    def intelligence_getter(self):
+        return self.intelligence
+    
+    def hunger_getter(self):
+        return self.hunger
+    
+    def strength_getter(self):
+        return self.strength
+    
+    def last_interaction_time_getter(self):
+        return self.last_interaction_time
+    
+    def saving_attributes(self):
+        saved_data=[]
+        saved_data.append(self.name_getter())
+        print(saved_data)
+        saved_data.append(self.health_getter())
+        print(saved_data)
+        saved_data.append(self.happiness_getter())
+        print(saved_data)
+        saved_data.append(self.hygiene_getter())
+        print(saved_data)
+        saved_data.append(self.age_getter())
+        print(saved_data)
+        saved_data.append(self.weight_getter())
+        print(saved_data)
+        saved_data.append(self.intelligence_getter())
+        print(saved_data)
+        saved_data.append(self.hunger_getter())
+        print(saved_data)
+        saved_data.append(self.strength_getter())
+        print(saved_data)
+        saved_data.append(self.last_interaction_time_getter())
+        print(saved_data)
+        file = open('stats.txt', 'w')
+        file.write(str(saved_data))
+        file.close() 
+        
+    
+    
 
-def save_state(self):
-    state = {
-        'name': self.name,
-        'health': self.health,
-        'happiness': self.happiness,
-        'hygiene': self.hygiene,
-        'age': self.age,
-        'weight': self.weight,
-        'intelligence': self
-    }
+thisTamagotchi= Tamagotchi("maddie")
+thisTamagotchi.saving_attributes()
+
+file = open('stats.txt', 'r')
+data = file.read() 
+# replacing end of line('/n') with ' ' and 
+# splitting the text it further when '.' is seen. 
+data_into_list = data.replace('\n', ' ').split(".") 
+# printing the data 
+print(data_into_list) 
+file.close() 
 
 
-thisTamagotchi=Tamagotchi("Maddie")
-print(update(thisTamagotchi))
+
+
+
+# file = open('stats.txt', 'w')
+# file.write(saved_name=thisTamagotchi.name_getter())
+# file.write(saved_health=thisTamagotchi.health_getter())
+# file.write(saved_happiness=thisTamagotchi.happiness_getter())
+# file.write(saved_hygiene=thisTamagotchi.hygiene_getter())
+# file.write(saved_age=thisTamagotchi.age_getter())
+# file.write(saved_weight=thisTamagotchi.weight_getter())
+# file.write(saved_intelligence=thisTamagotchi.intelligence_getter())
+# file.write(saved_hunger=thisTamagotchi.hunger_getter())
+# file.write(saved_strength=thisTamagotchi.strength_getter())
+# file.write(saved_last_interaction_time=thisTamagotchi.last_interaction_time_getter())
+# file.close()
+
+# import json
+
+# def save_state(self):
+#     state = {
+#         'name': self.name,
+#         'health': self.health,
+#         'happiness': self.happiness,
+#         'hygiene': self.hygiene,
+#         'age': self.age,
+#         'weight': self.weight,
+#         'intelligence': self
+#     }
+    
+         
+
+
